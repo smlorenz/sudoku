dfd2d0f68e9f09caa5c59dc9c4948ba4146b14be
smlorenz <smlorenz@knox.edu>
2021-05-11 17:23:40 -0500

    Getting it to work?

---
+++ smlorenzBoardGame/src/smlorenzBoardGame/Sudoku.java
@@ -0,0 +1,183 @@
+package smlorenzBoardGame;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Scanner;
+
+public class Sudoku {
+	
+	private String[][] board = new String[9][9];
+    
+    /**
+     * Get the num at rc
+     * @param row
+     * @param col
+     * @return The number at the given row and column.
+     */
+    public String get(int row, int col) {
+        if (row<0 || col<0 || row>=board.length || col>=board.length) {
+        	// should we throw an exception instead?
+            return null; 
+        }
+        return board[row][col];
+    }
+    
+    public void setNums(int r, int c, String n) {
+        board[r][c] = n;
+        if(n.equals("0")) {
+        	return;
+        }
+    }
+    
+    /**
+     * Load these numbers into a set.
+     * @param filename Name of the file to read.
+     * @throws IOException
+     */
+    
+    //TODO: nested for loops (0-9,,,,0-9)
+    public void loadNumbers(String filename)
+    throws IOException
+    {
+        Scanner scan = new Scanner(new FileInputStream(filename));
+        for (int r=0; r<9; r++) {
+          for (int c=0; c<9; c++) {
+        	  while(scan.hasNext()) {
+        		  String n = scan.next();
+        		  if (!n.equals("0")) {
+        			  this.setNums(r, c, n);
+        		  	}
+        	  	}
+          	}
+        }
+    }
+    
+    /**
+     * Creating a new Sudoku board that is blank.
+     * @param s is the numbers
+     * @throws IOException
+     */
+    
+    public void configureBoard() { //this is just making a blank board
+        for (int i=0; i<9; i++) {
+        	for (int j=0; j<9; j++) {
+            board[i][j] = "";
+        	}
+        }
+    }
+    
+    public Sudoku(String filename) //initialize board to blank (put 0 in every spot)
+    {
+        // load nums
+        try {
+        	loadNumbers(filename);
+        } catch (IOException e) {
+        	// no way to recover from this, so just repackage as runtime exception
+        	throw new RuntimeException(e);
+        }
+        //configureBoard();
+    }
+    
+    public void showSolution(String filename) throws IOException {
+    	this.loadNumbers(filename);
+    }
+    
+    public boolean difficultyLevel(String filename) {
+    	if(filename.equals("EasyPuzzle")) {
+    		return true;
+    	} else if(filename.equals("MediumPuzzle")) {
+    		return true;
+    	} else if(filename.equals("HardPuzzle")) {
+    		return true;
+    	} else if(filename.equals("EvilPuzzle")) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+  
+//    public Collection<Integer> findValidWords()
+//    {
+//        Set<String> result = new TreeSet<>();
+//        for (int r=0; r<4; r++) {
+//        	for (int c=0; c<4; c++) {
+//        		findHelper(r, c, new HashSet<String>(), "", result);
+//        	}
+//        }
+//    	
+//    	return result;
+//    }
+//    
+//    private String toCoord(int row, int col) {
+//    	return "" + row + col;
+//    }
+//    
+//    private void findHelper(int row, int col, Set<String> visited, String prefix, Set<String> result)
+//    {
+//        if (visited.contains(toCoord(row, col))) {
+//        	return;
+//        }
+//        
+//        if (!trie.startsWith(prefix)) {
+//        	return;
+//        }
+//        
+//        if (row < 0 || col < 0 || row > 3 || col > 3) {
+//        	return;
+//        }
+//        
+//        visited.add(toCoord(row, col));
+//    	
+//    	// we found a word!
+//        prefix += get(row, col);
+//    	if (trie.contains(prefix)) {
+//    		result.add(prefix);
+//    	}
+//    	
+//    	// recursive steps!
+//    	for (int r=row-1; r<=row+1; r++) {
+//    		for (int c=col-1; c<=col+1; c++) {
+//    			if (r == row && c == col) {
+//    				continue;
+//    			}
+//    			// new HashSet<String>(visited): "copy constructor"
+//    			findHelper(r, c, new HashSet<String>(visited), prefix, result);
+//    		}
+//    	}
+//    }
+//    
+//    /**
+//     * Shuffle the dice, assign them to a spot on the board, and roll them.  Use
+//     * the given random number generator for shuffling and rolling the dice.
+//     * 
+//     * Passing the Random so that we can get predictable results
+//     * 
+//     * @param r
+//     */
+//    public void configureBoard() {
+//        // Shuffle the dice
+//        shuffleDice();
+//        for (int i=0; i<dice.length; i++) {
+//            int row = i / board.length;
+//            int col = i % board.length;
+//            board[row][col] = dice[i].roll(random);
+//        }
+//    }
+//    
+//    /* (non-Javadoc)
+//     * @see java.lang.Object#toString()
+//     */
+//    public String toString() {
+//        StringBuffer buf=new StringBuffer();
+//        for (int row=0; row<board.length; row++) {
+//            for (int col=0; col<3; col++) {
+//                buf.append(board[row][col]);
+//                buf.append("\t");
+//            }
+//            buf.append(board[row][3]);
+//            buf.append("\n");
+//        }
+//        return buf.toString();
+//    }
+
+}
--- smlorenzBoardGame/src/smlorenzBoardGame/SudokuBoard.java
+++ smlorenzBoardGame/src/smlorenzBoardGame/SudokuBoard.java
@@ -1,5 +1,271 @@
 package smlorenzBoardGame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Random;
 
-public class SudokuBoard {
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
-}
+	public class SudokuBoard extends JFrame {
+	    private Random random;
+	  //TODO: add in this class
+	    private Sudoku sudoku; 
+	    
+		private static final long serialVersionUID = 1L;
+		
+		protected final int MARGIN_SIZE = 5;
+	    protected final int DOUBLE_MARGIN_SIZE = MARGIN_SIZE*2;
+	    protected int squareSize = 100;
+	    private int numRows = 9;
+	    private int numCols = 9;
+	    
+	    private int width = DOUBLE_MARGIN_SIZE + squareSize * numCols;    		
+	    private int height = DOUBLE_MARGIN_SIZE + squareSize * numRows;    		
+	    
+	    private JPanel canvas;
+	    private JMenuBar menuBar;
+	    
+		private void drawGrid(Graphics2D g) {
+			g.setColor(Color.BLACK);
+			Font font = new Font("Verdana", Font.BOLD, 40);
+			g.setFont(font);
+	        for (int r = 0; r < numRows; r++) {
+	            for (int c = 0; c < numCols; c++) {
+	                g.drawString(sudoku.get(r, c), 
+	                		c * squareSize + MARGIN_SIZE + squareSize/3, 
+	                		r * squareSize + MARGIN_SIZE + 2*squareSize/3);
+
+	                g.drawLine(MARGIN_SIZE+c*squareSize,
+	                        MARGIN_SIZE+r*squareSize,
+	                        MARGIN_SIZE+(c+1)*squareSize,
+	                        MARGIN_SIZE+r*squareSize);
+	                g.drawLine(MARGIN_SIZE+c*squareSize, 
+	                        MARGIN_SIZE+r*squareSize, 
+	                        MARGIN_SIZE+c*squareSize, 
+	                        MARGIN_SIZE+(r+1)*squareSize);
+	                g.drawLine(MARGIN_SIZE+(c+1)*squareSize, 
+	                        MARGIN_SIZE+r*squareSize, 
+	                        MARGIN_SIZE+(c+1)*squareSize, 
+	                        MARGIN_SIZE+(r+1)*squareSize);
+	                g.drawLine(MARGIN_SIZE+c*squareSize, 
+	                        MARGIN_SIZE+(r+1)*squareSize, 
+	                        MARGIN_SIZE+(c+1)*squareSize, 
+	                        MARGIN_SIZE+(r+1)*squareSize);
+	            }
+	        }
+	        
+	    }
+		
+		//TODO: new game? reset? hints?
+	    
+	    private void createMenuBar() {
+	    	menuBar = new JMenuBar();
+	        JMenu menu = new JMenu("Actions");
+	        menuBar.add(menu);
+	        
+	        JMenu difficultyMenu = new JMenu("Difficulty");
+	        menuBar.add(difficultyMenu);
+	        
+	        addToMenu(difficultyMenu, "Easy", new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                try {
+						sudoku.loadNumbers("EasyPuzzle");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	                repaint();
+	            }
+	        });
+	        
+	        addToMenu(difficultyMenu, "Medium", new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                try {
+						sudoku.loadNumbers("MediumPuzzle");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	                repaint();
+	            }
+	        });
+	        
+	        addToMenu(difficultyMenu, "Hard", new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                try {
+						sudoku.loadNumbers("HardPuzzle");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	                repaint();
+	            }
+	        });
+	        
+	        addToMenu(difficultyMenu, "EVIL", new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                try {
+						sudoku.loadNumbers("EvilPuzzle");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	                repaint();
+	            }
+	        });
+	        
+	        JMenu solutionMenu = new JMenu("Solutions");
+	        menuBar.add(solutionMenu);
+	        
+	        addToMenu(solutionMenu, "Show solution", new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	            	if(sudoku.difficultyLevel("EasyPuzzle")) {
+	                try {
+						sudoku.showSolution("EasyPuzzleSolutions");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	            	}
+	            	if(sudoku.difficultyLevel("MediumPuzzle")) {
+		                try {
+							sudoku.showSolution("MediumPuzzleSolutions");
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+		            }
+	            	if(sudoku.difficultyLevel("HardPuzzle")) {
+		                try {
+							sudoku.showSolution("HardPuzzleSolutions");
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+		            }
+	            	if(sudoku.difficultyLevel("EvilPuzzle")) {
+		                try {
+							sudoku.showSolution("EvilPuzzleSolutions");
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+		            }
+	            }
+	        });
+	        this.setJMenuBar(menuBar);
+	    }
+	    
+	    private void addToMenu(JMenu menu, String title, ActionListener listener) {
+	    	JMenuItem menuItem = new JMenuItem(title);
+	    	menu.add(menuItem);
+	    	menuItem.addActionListener(listener);
+	    }
+	    
+	    private void createKeyboardHandlers() {
+	    	this.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyTyped(KeyEvent e) {
+					System.out.println(e.getKeyChar());
+				}
+			});
+	    }
+	    
+	    public SudokuBoard() {
+	        //random = new Random(299);
+	        sudoku = new Sudoku("EasyPuzzle");
+	        sudoku.configureBoard();
+	        
+	        setTitle("Sudoku!");
+
+	        JFrame frame = this;
+	        
+	        canvas = new JPanel() {
+	        	 /* (non-Javadoc)
+	             * @see javax.swing.JComponent#getMinimumSize()
+	             */
+	            public Dimension getMinimumSize() {
+	                return new Dimension(width, height);
+	            }
+	            
+	            /* (non-Javadoc)
+	             * @see javax.swing.JComponent#getMaximumSize()
+	             */
+	            public Dimension getMaximumSize() {
+	                return new Dimension(width, height);
+	            }
+	            
+	            /* (non-Javadoc)
+	             * @see javax.swing.JComponent#getPreferredSize()
+	             */
+	            public Dimension getPreferredSize() {
+	                return new Dimension(width, height);
+	            }
+	            
+	            /* (non-Javadoc)
+	             * @see java.awt.Component#isFocusable()
+	             */
+	            public boolean isFocusable() {
+	                return true;
+	            }
+
+				@Override
+	        	public void paint(Graphics graphics) {
+	        		Graphics2D g = (Graphics2D)graphics;
+
+	        		drawGrid(g);
+
+	        		//frame.setPreferredSize(new Dimension(numRows*squareSize + MARGIN_SIZE, numCols*squareSize + MARGIN_SIZE));
+	        		setPreferredSize(new Dimension((numCols+2)*squareSize + 2*MARGIN_SIZE, (numRows+2)*squareSize + 2*MARGIN_SIZE));
+	        		frame.pack();
+	        	}
+	        };
+	        
+	        this.getContentPane().add(canvas, BorderLayout.CENTER);
+	        this.setResizable(false);
+	        this.pack();
+	        this.setLocation(100,100);
+	        this.setFocusable(true);
+	        
+	        createMenuBar();
+	        createKeyboardHandlers();
+	        
+	        addWindowListener(new WindowAdapter() {
+	            public void windowClosing(WindowEvent e) {
+	                System.exit(0);
+	            }
+	        });
+
+	        
+	        repaint();
+	    }
+	    
+	    public static void main(String[] args) {
+	        SudokuBoard s = new SudokuBoard();
+	        s.setVisible(true);
+	    }
+
+	}
