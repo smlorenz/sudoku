45d04b84bf9223383efb38f4136c7652d2a0c97a
smlorenz <smlorenz@knox.edu>
2021-05-23 12:13:29 -0500

    fix exception

--- smlorenzBoardGame/src/smlorenzBoardGame/IllegalNumberException.java
+++ smlorenzBoardGame/src/smlorenzBoardGame/IllegalNumberException.java
@@ -3,6 +3,5 @@ package smlorenzBoardGame;
 public class IllegalNumberException extends RuntimeException {
 	public IllegalNumberException(String mesg) {
 		super(mesg);
-		System.out.println(mesg);
 	}
 }
